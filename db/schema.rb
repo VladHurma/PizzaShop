4d88dafb314fcdf2c044c2fb34740c19d8f0be09
VladHurma <vladik_shyshko@mail.ru>
2019-01-13 09:57:55 +0300

    Rename igames and add seedlike migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_12_232134) do
+ActiveRecord::Schema.define(version: 2019_01_12_233342) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
