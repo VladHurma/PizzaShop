14e56913ad09d416a268fbdbda623527a58eec98
VladHurma <vladik_shyshko@mail.ru>
2019-01-13 11:05:24 +0300

    Refact seedlike migration, add one more pizza in seed, add sipmle grid for pizzas list

--- db/migrate/20190112233342_add_products.rb
+++ db/migrate/20190112233342_add_products.rb
@@ -1,9 +1,43 @@
 class AddProducts < ActiveRecord::Migration[5.2]
   def change
-    Product.create :title => 'Vegan', :description => 'This is vegan pizza', :price => 14, :size => 31, :is_spicy => false, :is_veg => true, :is_best_offer => false, :path_to_image => '/images/vegan.jpg'
+    Product.create ({
+      :title => 'Vegan',
+      :description => 'This is vegan pizza',
+      :price => 14,
+      :size => 31,
+      :is_spicy => false,
+      :is_veg => true,
+      :is_best_offer => false,
+      :path_to_image => '/images/vegan.jpg'})
 
-    Product.create :title => 'Ranch', :description => 'Classical ranch, just like you eat in childhood', :price => 18, :size => 31, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/images/ranch.jpg'
+    Product.create ({
+      :title => 'Ranch',
+      :description => 'Classical ranch, just like you eat in childhood',
+      :price => 18,
+      :size => 31,
+      :is_spicy => false,
+      :is_veg => false,
+      :is_best_offer => false,
+      :path_to_image => '/images/ranch.jpg'})
 
-    Product.create :title => 'Pepperoni', :description => 'Double pepperoni pizza', :price => 16, :size => 31, :is_spicy => true, :is_veg => false, :is_best_offer => true, :path_to_image => '/images/pepperoni.jpg'
+    Product.create ({
+      :title => 'Pepperoni',
+      :description => 'Double pepperoni pizza',
+      :price => 16,
+      :size => 31,
+      :is_spicy => true,
+      :is_veg => false,
+      :is_best_offer => true,
+      :path_to_image => '/images/pepperoni.jpg'})
+
+    Product.create ({
+      :title => 'Diablo',
+      :description => 'Hot as hell',
+      :price => 18,
+      :size => 31,
+      :is_spicy => true,
+      :is_veg => false,
+      :is_best_offer => false,
+      :path_to_image => '/images/diablo.jpg'})
   end
 end
