c00d9a9fe948f21716f56ad9d31768e8e10e6a21
VladHurma <vladik_shyshko@mail.ru>
2019-01-13 23:46:56 +0300

    Add id sender in cart and add function for cart

--- public/app.js
+++ public/app.js
@@ -1,14 +1,12 @@
 function something() {
-
   var x = window.localStorage.getItem('aaa');
-
-  x = x + 1;
-
   window.localStorage.setItem('aaa', x);
-
-  alert (x);
 }
 
-function add_to_cart() {
-  alert('Successfuly added to cart')
+function add_to_cart(id) {
+  var key = 'product_' + id;
+
+  var x = window.localStorage.getItem(key);
+  x = x * 1 + 1;
+  window.localStorage.setItem(key, x);
 }
