f204592dae0f08d123c16527fefd64e341b12b1a
VladHurma <vladik_shyshko@mail.ru>
2019-01-14 19:08:27 +0300

    Add js function for amount of items in cart

--- public/app.js
+++ public/app.js
@@ -5,3 +5,20 @@ function add_to_cart(id) {
   x = x * 1 + 1;
   window.localStorage.setItem(key, x);
 }
+
+function amount_of_products_in_cart() {
+
+  var total = 0;
+
+  for(var i = 0; i < window.localStorage.length; i++) {
+
+    var key = window.localStorage.key(i);
+
+    if (key.indexOf('product_') == 0) {
+       total += (window.localStorage.getItem(key) * 1);
+    }
+
+  }
+
+  return total;
+}
